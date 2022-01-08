fe189df1190b2d9d5f8f6a91c55e201e3266a103
CyberRye <cyberrye@163.com>
2022-01-08 15:50:55 +0300

    Use RecyclerView to show data dynamically.

--- app/src/main/java/com/example/myapplication/HolidayAdapter.java
+++ app/src/main/java/com/example/myapplication/HolidayAdapter.java
@@ -1,42 +1,128 @@
 package com.example.myapplication;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class HolidayAdapter extends RecyclerView.Adapter {
+public class HolidayAdapter extends RecyclerView.Adapter<HolidayAdapter.MyViewHolder> {
 
-    private List<Holiday> dataList;
+    private List<Holiday> holidayList;
+    private LayoutInflater mLayoutInflater;
+    private Context mContext;
 
-    public HolidayAdapter(List<Holiday> dataList){
-        this.dataList = dataList;
+
+    public HolidayAdapter(Context context, List<Holiday> holidayList) {
+        this.mContext = context;
+        this.holidayList = holidayList;
+        mLayoutInflater = LayoutInflater.from(context);
     }
+
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mLayoutInflater.inflate(R.layout.itemlayout, parent, false);
+
+        MyViewHolder myViewHolder = new MyViewHolder(view);
+
+        return myViewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-//        holder.
-//        item.put("date", holiday.getDate());
-//        item.put("localName", holiday.getLocalName());
-//        item.put("name", holiday.getName());
-//        item.put("countryCode", holiday.getCountryCode());
-//        item.put("fixed", holiday.isFixed());
-//        item.put("global", holiday.isGlobal());
-//        item.put("counties", holiday.getCountries());
-//        item.put("launchYear", holiday.getLaunchYear());
-//        item.put("type", holiday.getType());
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Holiday holiday = holidayList.get(position);
+
+        holder.mDate.setText(holiday.getDate());
+        holder.mName.setText(holiday.getName());
+        holder.mLocalName.setText(holiday.getLocalName());
+        holder.mCountryCode.setText(holiday.getCountryCode());
+        holder.mType.setText(holiday.getType());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return holidayList.size();
     }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mDate;
+        TextView mLocalName;
+        TextView mName;
+        TextView mCountryCode;
+        TextView mType;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.mDate = itemView.findViewById(R.id.textDate);
+            this.mName = itemView.findViewById(R.id.textName);
+            this.mLocalName = itemView.findViewById(R.id.textLocalName);
+            this.mCountryCode = itemView.findViewById(R.id.textCountryCode);
+            this.mType = itemView.findViewById(R.id.textType);
+        }
+    }
+
+//    private Context context;
+//    private ArrayList<Holiday> dataList;
+//
+//    public HolidayAdapter(Context mContext, ArrayList<Holiday> dataList) {
+//        this.dataList = dataList;
+//        this.context = mContext;
+//    }
+//
+//    public HolidayAdapter() {
+//    }
+//
+//    @Override
+//    public HolidayHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+//        View v = LayoutInflater.from(context).inflate(R.layout.itemlayout, parent, false);
+//
+//        return new HolidayHolder(v);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull HolidayAdapter.HolidayHolder holder, int position) {
+//        Holiday holiday = dataList.get(position);
+//        System.out.println("xxxxxxxxxxxxx" + holiday.getDate());
+//        System.out.println("xxxxxxxxxxxxx" + holiday.getName());
+//        System.out.println("xxxxxxxxxxxxx" + holiday.getCountries());
+//        System.out.println("xxxxxxxxxxxxx" + holder);
+//        System.out.println("xxxxxxxxxxxxx" + holder.textDate);
+////        holder.date.setText(holiday.getDate());
+//        holder.textDate = new TextView(this.context);
+//        holder.textDate.setText(holiday.getDate());
+//        holder.textName = new TextView(this.context);
+//        holder.textName.setText(holiday.getName());
+//        System.out.println("11111111111111    " + holder);
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return this.dataList.size();
+//    }
+//
+//
+//    class HolidayHolder extends RecyclerView.ViewHolder {
+//        public TextView textDate;
+//        public TextView textName;
+//
+//        //    public HolidayHolder(LayoutInflater inflater, ViewGroup parent) {
+////        super(inflater.inflate(R.layout.itemlayout, parent, false));
+////    }
+//        public HolidayHolder(View itemView) {
+//            super(itemView);
+//            textDate = (TextView) itemView.findViewById(R.id.textDate);
+//            textName = (TextView) itemView.findViewById(R.id.textName);
+//        }
+//
+//    }
+
 }
+
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -20,22 +20,109 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private Button mGetBtn = null;
+    private RecyclerView recyclerView;
+    private ArrayList<Holiday> holidayList;
+    private HolidayAdapter holidayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        this.setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_main);
         if (Build.VERSION.SDK_INT > 9) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
+        initView();
+        iniData();
+        mGetBtn = findViewById(R.id.button);
+        mGetBtn.setOnClickListener(new View.OnClickListener() {
 
-        setContentView(R.layout.holidays);
-        mGetBtn = (Button) findViewById(R.id.get_btn);
-        String result = getHtml("test");
-        ListView listView = (ListView) findViewById(R.id.list_test);
-        final List<Holiday> adapterData = new ArrayList<Holiday>();
+            @Override
+            public void onClick(View view) {
+                initEvent();
+            }
+        });
+
+//        ArrayList<Holiday> adapterData = getHolidayList();
+//        holidayAdapter = new HolidayAdapter(this, adapterData);
+//
+//        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+//        recyclerView.setAdapter(holidayAdapter);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+
+
+//        setContentView(R.layout.itemlayout);
+//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+//        HolidayAdapter adapter = new HolidayAdapter(this, (ArrayList<Holiday>) adapterData);
+//        RecyclerView.Adapter = adapter;
+
+
+//        mGetBtn = (Button) findViewById(R.id.get_btn);
+//        List<Holiday> adapterData = getHolidayList();
+//
+//        List<HashMap<String, Object>> data = buildAdapter(adapterData);
+//        SimpleAdapter adapter = new SimpleAdapter(getApplicationContext(),
+//                data, R.layout.holidays, new String[]{"date", "localName", "name", "countryCode", "type"},
+//                new int[]{R.id.date, R.id.localName, R.id.name, R.id.countryCode, R.id.type});
+//        mGetBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                ListView listView = findViewById(R.id.list_test);
+//                final List<String> adapterData = new ArrayList<String>();
+//                for (int i = 0; i < 20; i++) {
+//                    adapterData.add("ListItem" + i);
+//                }
+//                listView.setAdapter(adapter);
+//            }
+//        });
+
+    }
+
+    public void initEvent() {
+        holidayAdapter = new HolidayAdapter(this, holidayList);
+        recyclerView.setAdapter(holidayAdapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+    }
+
+    private void iniData() {
+
+        holidayList = getHolidayList();
+
+    }
+
+    private void initView() {
+        recyclerView = findViewById(R.id.recyclerView);
+    }
+
+    public static void getComments(View view) {
+
+    }
+
+    public static List<HashMap<String, Object>> buildAdapter(List<Holiday> adapterData) {
+        List<HashMap<String, Object>> data = new ArrayList<HashMap<String, Object>>();
+        for (Holiday holiday : adapterData) {
+            HashMap<String, Object> item = new HashMap<String, Object>();
+            item.put("date", holiday.getDate());
+            item.put("localName", holiday.getLocalName());
+            item.put("name", holiday.getName());
+            item.put("countryCode", holiday.getCountryCode());
+            item.put("fixed", holiday.isFixed());
+            item.put("global", holiday.isGlobal());
+            item.put("counties", holiday.getCountries());
+            item.put("launchYear", holiday.getLaunchYear());
+            item.put("type", holiday.getType());
+            data.add(item);
+        }
+        return data;
+    }
+
+    public static ArrayList<Holiday> getHolidayList() {
+        String test = "https://date.nager.at/api/v2/publicholidays/2022/BY";
+
+        String result = getHtml(test);
+        final ArrayList<Holiday> adapterData = new ArrayList<Holiday>();
 
         try {
             JSONArray elemList = new JSONArray(result);
@@ -57,44 +144,14 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        List<HashMap<String, Object>> data = new ArrayList<HashMap<String, Object>>();
-        for (Holiday holiday : adapterData) {
-            HashMap<String, Object> item = new HashMap<String, Object>();
-            item.put("date", holiday.getDate());
-            item.put("localName", holiday.getLocalName());
-            item.put("name", holiday.getName());
-            item.put("countryCode", holiday.getCountryCode());
-            item.put("fixed", holiday.isFixed());
-            item.put("global", holiday.isGlobal());
-            item.put("counties", holiday.getCountries());
-            item.put("launchYear", holiday.getLaunchYear());
-            item.put("type", holiday.getType());
-            data.add(item);
-        }
-        SimpleAdapter adapter = new SimpleAdapter(getApplicationContext(),
-                data, R.layout.holidays, new String[]{"date", "localName", "name", "countryCode", "type"},
-                new int[]{R.id.date, R.id.localName, R.id.name, R.id.countryCode, R.id.type});
-        mGetBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                ListView listView = findViewById(R.id.list_test);
-                final List<String> adapterData = new ArrayList<String>();
-                for (int i = 0; i < 20; i++) {
-                    adapterData.add("ListItem" + i);
-                }
-                listView.setAdapter(adapter);
-            }
-        });
-
+        return adapterData;
     }
 
     public static String getHtml(String url) {
         HtmlService service = new HtmlService();
-        String test = "https://date.nager.at/api/v2/publicholidays/2022/BY";
         String result = "123";
         try {
-            result = service.getHtml(test);
+            result = service.getHtml(url);
         } catch (Exception e) {
             e.printStackTrace();
         }
